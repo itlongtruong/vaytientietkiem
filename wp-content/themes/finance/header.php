513d03a463994dfb0e440fd90aac58f1100dcd0e
craft-long <truonglong12a1@gmail.com>
2022-07-15 15:16:47 +0900

    20220715

--- wp-content/themes/finance/header.php
+++ wp-content/themes/finance/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header.
  *
@@ -14,33 +15,34 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<?php wp_head(); ?>
-	<!-- Favicons -->
-    <link href="<?php echo get_template_directory_uri();?>/assets/img/favicon.png" rel="icon">
-    <link href="<?php echo get_template_directory_uri();?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-  	<!-- Google Fonts -->
-  	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet">
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <?php wp_head(); ?>
+  <!-- Favicons -->
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/img/favicon.png" rel="icon">
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Montserrat:300,400,500,600,700" rel="stylesheet">
 
-  	<!-- Vendor CSS Files -->
-  	<link href="<?php echo get_template_directory_uri();?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-	  <link href="<?php echo get_template_directory_uri();?>/assets/vendor/bootstrap/css/bootstrap-grid.css" rel="stylesheet">
-	<link href="<?php echo get_template_directory_uri();?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-	<link href="<?php echo get_template_directory_uri();?>/assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-  	<link href="<?php echo get_template_directory_uri();?>/assets/vendor/ionicons/css/ionicons.min.css" rel="stylesheet">
-  	<link href="<?php echo get_template_directory_uri();?>/assets/vendor/venobox/venobox.css" rel="stylesheet">
-  	<link href="<?php echo get_template_directory_uri();?>/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-  	<link href="<?php echo get_template_directory_uri();?>/assets/vendor/aos/aos.css" rel="stylesheet">
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/vendor/bootstrap/css/bootstrap-grid.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/vendor/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/vendor/aos/aos.css" rel="stylesheet">
 
-  	<!-- Template Main CSS File -->
-  	<link href="<?php echo get_template_directory_uri();?>/assets/css/style.css" rel="stylesheet">   
-  	<link href="<?php echo get_template_directory_uri();?>/assets/css/custom.css" rel="stylesheet">
+  <!-- Template Main CSS File -->
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/assets/css/custom.css" rel="stylesheet">
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
+  <?php wp_body_open(); ?>
   <!-- ======= Top Bar ======= -->
   <div id="topbar" class="d-none d-lg-flex align-items-end fixed-top">
     <div class="container d-flex justify-content-end">
@@ -51,6 +53,7 @@
         <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
       </div>
     </div>
+
   </div>
 
   <!-- ======= Header ======= -->
@@ -59,23 +62,27 @@
 
       <!-- <h1 class="logo mr-auto"><a href="index.html">Rapid</a></h1> -->
       <!-- Uncomment below if you prefer to use an image logo -->
-       <a href="<?php echo home_url(); ?>/" class="logo mr-auto"><img src="<?php echo get_template_directory_uri();?>/assets/img/logo.png" alt="" class="img-fluid"></a>
-      <nav  class="main-nav d-none d-lg-block">
-	  
-		<?php wp_nav_menu( 
-		array( 
-			'theme_location' => 'header-menu', 
-			'container' => 'false', 
-			'menu_id' => 'header-menu', 
-			'menu_class' => 'menu'
-		) 
-		); ?>
+      <a href="<?php echo home_url(); ?>/" class="logo mr-auto"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="" class="img-fluid"></a>
+      <nav class="main-nav d-none d-lg-block">
 
-		
-
+        <?php wp_nav_menu(
+          array(
+            'theme_location' => 'header-menu',
+            'container' => 'false',
+            'menu_id' => 'header-menu',
+            'menu_class' => 'menu'
+          )
+        ); ?>
+       
       </nav><!-- .main-nav-->
-	 
+
+
+      <form action="<?php bloginfo('url'); ?>/" method="GET" role="search" class="search-bar d-none d-lg-block">
+        <input type="search" name="s" required <?php if (is_search()) { ?>value="<?php the_search_query(); ?>" <?php } else { ?>value="" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;" <?php } ?>>
+        <button class="search-btn" type="submit">
+          <span >Search</span>
+        </button>
+      </form>
 
     </div>
   </header><!-- End Header -->
-
